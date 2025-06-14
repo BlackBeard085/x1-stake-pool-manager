d8287cd52315bf4054313dcdd9a4fe29b7e6adab
BlackBeard085 <Blackbeard.x1Val@gmail.com>
2025-06-14 18:48:14 +0000

    added failed to add/remove Val retry logic to add/remove pool Validators

--- add-validators.sh
+++ add-validators.sh
@@ -1,18 +1,41 @@
 #!/usr/bin/env bash
 
 # Script to add new validators to a stake pool, given the stake pool keyfile and
-# a file listing validator vote account pubkeys
+# a file listing validator vote account pubkeys with retry logic and success checks
 
 cd "$(dirname "$0")" || exit
 stake_pool_keyfile=$1
 validator_list=$2  # File containing validator vote account addresses, each will be added to the stake pool after creation
 
 add_validator_stakes () {
-  stake_pool=$1
-  validator_list=$2
+  local stake_pool=$1
+  local validator_list=$2
+  local max_retries=5
+
   while read -r validator
   do
-    $spl_stake_pool add-validator "$stake_pool" "$validator"
+    echo "Attempting to add validator: $validator"
+
+    local attempt=1
+    local success=0
+
+    until [ $attempt -gt $max_retries ]
+    do
+      echo "Attempt #$attempt for validator: $validator"
+      if "$spl_stake_pool" add-validator "$stake_pool" "$validator"; then
+        echo "Successfully added validator: $validator"
+        success=1
+        break
+      else
+        echo "Failed to add validator: $validator on attempt #$attempt" >&2
+        attempt=$((attempt + 1))
+        sleep 2  # Optional: wait before retrying
+      fi
+    done
+
+    if [ $success -eq 0 ]; then
+      echo "Failed to add validator after $max_retries attempts: $validator" >&2
+    fi
   done < "$validator_list"
 }
 
@@ -28,10 +51,12 @@ full_command=$(eval echo "$command_path")
 # Assign to variable
 spl_stake_pool="$full_command"
 
-#spl_stake_pool=spl-stake-pool
-# Uncomment to use a local build
-#spl_stake_pool=../../../target/debug/spl-stake-pool
+# Uncomment and set if you want to use a local build
+# spl_stake_pool=../../../target/debug/spl-stake-pool
 
+# Derive the stake pool's public key
 stake_pool_pubkey=$(solana-keygen pubkey "$stake_pool_keyfile")
-echo "Adding validator stake accounts to the pool"
+echo "Adding validator stake accounts to the pool with pubkey: $stake_pool_pubkey"
+
+# Call the function to add validators with retry logic
 add_validator_stakes "$stake_pool_pubkey" "$validator_list"
